09aab21b55fd7ab6cd00d4f6eb974b879653e8ba
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-22 22:12:34 +0530

    Auto-commit: 2025-08-22 22:12:34

---
+++ AdamNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class AdamNumber{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int sqr = n*n;
+        int rev =0;
+        while(n>0){
+            int digit = n%10;
+             rev = rev*10+digit;
+            n= n/10;
+        }
+        
+        int sq_rev = rev*rev;
+        int rev1 =0;
+        while(sq_rev>0){
+            int v = sq_rev%10;
+             rev1 = rev1*10+v;
+            sq_rev = sq_rev/10;
+        }
+        if(sqr == rev1){
+            System.out.println("ADAM");
+        }
+        else{
+            System.out.println("NOT ADAM");
+        }
+       
+    }
+
+}
