8ae8c7eb4354691c6a26594394d73b1f2effd632
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 14:16:32 +0530

    Auto-commit: 2025-08-23 14:16:32

--- frontSpace.java
+++ frontSpace.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class frontSpace{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in
-;   }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENter the 
+        int n = sc.nextInt();
+   }
 }
