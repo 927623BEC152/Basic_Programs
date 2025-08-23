ff74f3033c2e404e0192e18a57afad778dc7d4c0
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 15:44:35 +0530

    Auto-commit: 2025-08-23 15:44:34

--- Pyramid.java
+++ Pyramid.java
@@ -1,3 +1,23 @@
-public class Pyramid {
+ import java.util.*;
+ public class Pyramid {
+   
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the pattern size");
+              int n = sc.nextInt();
+              for(int row=1;row<=n;row++){
+                for(int col=1;col<=n-row;col++){
+                    System.out.print(" ");
+                }
+                for(int col=1;col<=row;col++){
+                    System.out.print("*");
+
+                }
+                System.out.println();
+            } 
+     sc.close();          
+   }
+  
+}
     
 }
