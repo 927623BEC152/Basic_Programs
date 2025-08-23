4972a88633b5c347258420ab370fcb29d8feb41d
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 10:07:49 +0530

    Auto-commit: 2025-08-23 10:07:49

--- ArrayInsertion.java
+++ ArrayInsertion.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class ArrayInsertion {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
         int num=sc.nextInt();
         int arr[]=new int[num];
         for(int i=0;i<num;i++)
