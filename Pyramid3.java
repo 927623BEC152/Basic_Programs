b7e8c583a1b49703152e2266be835916d89c008f
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 15:50:56 +0530

    Auto-commit: 2025-08-23 15:50:56

--- Pyramid3.java
+++ Pyramid3.java
@@ -1,5 +1,5 @@
-pmport java.util.*;
- public class Pyramid {
+import java.util.*;
+ public class Pyramid3 {
    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
