957227cdda1ce990d196d58c8d7efa3782868eed
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 12:54:57 +0530

    Auto-commit: 2025-08-23 12:54:57

--- IncrementPattern.java
+++ IncrementPattern.java
@@ -6,7 +6,7 @@ public class IncrementPattern {
         int n=sc.nextInt();
         for(int row=1;row<n;row++){
             for(int col=1;col<=row;col++){
-                System.out.print("* ");
+                System.out.print(col);
             }
             System.out.println();
         }
