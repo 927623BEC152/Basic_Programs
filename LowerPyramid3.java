aedd7e4bbb9ad4d83c5b95569f06fb1f12b54025
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 16:02:20 +0530

    Auto-commit: 2025-08-23 16:02:19

--- LowerPyramid3.java
+++ LowerPyramid3.java
@@ -3,7 +3,19 @@ public class LowerPyramid3 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Elements: ");
-        int n=s
+        int n=sc.nextInt();
+         for(int row=n;row>=1;row--)
+            {
+                for(int col=1;col<=n-row;col++){
+                    System.out.print(" ");
+                }
+                for(int col=1;col<=2*row-1;col++){
+                    System.out.print("1");
+
+                }
+                System.out.println();
+            } 
+        
     }
     
 }
