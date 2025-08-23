a33e77135fbec7e63e09755ba98436128705c17d
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 11:48:57 +0530

    Auto-commit: 2025-08-23 11:48:57

--- AdamNumber.java
+++ AdamNumber.java
@@ -25,7 +25,7 @@ public class AdamNumber{
         else{
             System.out.println("NOT ADAM");
         }
-       
+       sc.close();
     }
 
 }
--- Vowels.java
+++ Vowels.java
@@ -14,6 +14,7 @@ public class  Vowels{
         else{
             System.out.println("Consonant");
         }
+        
     }
 }
     
