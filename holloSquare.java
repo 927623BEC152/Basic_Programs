a26faea065b7109d349bb8edd6bcce7ebeb012c3
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 12:33:18 +0530

    Auto-commit: 2025-08-23 12:33:17

--- holloSquare.java
+++ holloSquare.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class holloSquare{
     public static void main(String[] args){
-        Scanner sc = new Scanner(null)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the pattern");
+        
     }
 
 }
