cace091c3c5708c5e77d2a3038f980be94205aff
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 12:35:42 +0530

    Auto-commit: 2025-08-23 12:35:42

--- holloSquare.java
+++ holloSquare.java
@@ -6,7 +6,9 @@ public class holloSquare{
         int n = sc.nextInt();
         for(int i=1;i<n;i++){
             for(int j=1;j<n;j++){
-
+               if(i==1||){
+                
+               }
             }
         }
     }
