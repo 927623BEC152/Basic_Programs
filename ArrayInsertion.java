2d0050e558f0b3cb1b539da1cead1aadb9f19438
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-23 10:04:30 +0530

    Auto-commit: 2025-08-23 10:04:30

--- ArrayInsertion.java
+++ ArrayInsertion.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class ArrayInsertion {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int num=
+        int num=sc.nextInt();
+        int arr[]=new int[]
     }
 
     
