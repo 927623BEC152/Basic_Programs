51728a0521dbf5cb71605d7707791c1d2a9c6127
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-30 10:31:24 +0530

    program

--- number_rotate.java
+++ number_rotate.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 public class number_rotate {
-    
-    
+       public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int  Num = sc.nextInt();
+        System.out.println(3-Num);
+        sc.close();
+    }
 }
+    
